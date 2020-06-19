2fc954e38c15088d7f57f911743d8c9bcb0cc63c
Archan Patkar <pat@archanpatkar.com>
2020-06-19 23:50:30 +0530

    Update

--- src/operators.js
+++ src/operators.js
@@ -37,8 +37,8 @@ const reduce = (operator,seed=undefined) => (observable) => new Observable(
         let acc = seed;
         let index = 0;
         observable.subscribe(
-            (v) => (acc = operator(acc,value,index)),
-            (e) => observer.error(e),
+            v => (acc = operator(acc,value,index)),
+            e => observer.error(e),
             () => {
                 observer.next(acc);
                 observer.complete();
@@ -51,17 +51,55 @@ const buffer = (buffered) => (observable) => new Observable(
     (observer) => {
         let internalBuff = [];
         buffered.subscribe(
-            (v) => internalBuff.push(v),
-            e => observer.error(e),
-            () => observer.complete()
+            v => internalBuff.push(v),
+            e => observer.error(e)
         );
         observable.subscribe(
-            (v) => {
+            v => {
                 observer.next(internalBuff)
                 internalBuff = []
             },
             e => observer.error(e),
-            () => observer.complete()
+            () => {
+                observer.next(internalBuff)
+                observer.complete()
+            }
+        )
+    }
+);
+
+// WIP
+const bufferCount = (bufferSize, startBufferEvery = null) => (observable) => new Observable(
+    (observer) => {
+        let buffs = [[]];
+        let count = 0;
+        observable.subscribe(
+            v => {
+                if(startBufferEvery){
+                    let remove = [];
+                    buffs.forEach((b,i) => 
+                        b.length == bufferSize? !observer.next(b) && remove.push(i): b.push(v)
+                    );
+                    buffs = buffs.filter((b,i) => !remove.includes(i))
+                    count++;
+                    if(count == startBufferEvery-1){
+                        buffs.push([])
+                        count = 0;
+                    }
+                }
+                else {
+                    if(buffs[0].length < bufferSize) buffs[0].push(v)
+                    else {
+                        observer.next(buffs[0])
+                        (buffs[0] = []).push(v)
+                    }
+                }
+            },
+            e => observer.error(e),
+            () => {
+                observer.next(internalBuff)
+                observer.complete()
+            }
         )
     }
 );
